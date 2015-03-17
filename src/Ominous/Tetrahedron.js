23b4e4261d14328cb3540f255aa9ea786c468ca6
Gabriella <gabriella.karin@gmail.com>
2015-03-17 08:57:54 +0100

    Readd movement to tetrahedron.

--- src/Ominous/Tetrahedron.js
+++ src/Ominous/Tetrahedron.js
@@ -1,11 +1,8 @@
 'use strict';
 
+import CANNON from 'cannon';
 import THREE from 'three';
 import Glow from './Glow';
-
-var CANNON = {};
-CANNON.Body = require('cannon/src/objects/Body');
-CANNON.Vec3 = require('cannon/src/math/Vec3');
 
 var DateNow = Date.now;
 var cos = Math.cos;
@@ -28,55 +25,55 @@ export default class Tetrahedron extends THREE.Mesh {
 
         super( geometry, material );
 
-        this._body = new Body( 70 );
+        this.body = new Body( 70 );
 
         this.glow = new Glow({
 
             color: options.color,
-            position: this._body.position,
+            position: this.body.position,
             viewVector: options.viewVector,
             geometry: geometry
 
         });
 
-        this._raycaster = new THREE.Raycaster();
-        this._mouse = new THREE.Vector2();
+        this.raycaster = new THREE.Raycaster();
+        this.mouse = new THREE.Vector2();
 
-        this._bindEvents();
-
-    }
-
-    _bindEvents() {
-
-        window.addEventListener( 'mousemove', this._handleMouseMove.bind(this) );
-        window.addEventListener( 'touchmove', this._handleTouchMove.bind(this) );
+        this.bindEvents();
 
     }
 
-    _handleMouseMove( event ) {
+    bindEvents() {
 
-        this._mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        this._mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        window.addEventListener( 'mousemove', this.handleMouseMove.bind(this) );
+        window.addEventListener( 'touchmove', this.handleTouchMove.bind(this) );
 
     }
 
-    _handleTouchMove( event ) {
+    handleMouseMove( event ) {
+
+        this.mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        this.mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+    }
+
+    handleTouchMove( event ) {
 
         if ( event.touches && event.touches.length > 0 ) {
-            this._handleMouseMove( event.touches[0] );
+            this.handleMouseMove( event.touches[0] );
         }
 
     }
 
     update( camera ) {
 
-        var raycaster = this._raycaster;
-        var body = this._body;
+        var raycaster = this.raycaster;
+        var body = this.body;
 
         body.update( this );
         this.glow.update( camera, body );
 
-        raycaster.setFromCamera( this._mouse, camera );
+        raycaster.setFromCamera( this.mouse, camera );
 
         if ( raycaster.intersectObject( this ).length ) {
 
